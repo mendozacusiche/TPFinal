6fcfc626b78ad32a0d44392e3c2b90893a2ca6dd
AgusEchaniz <62100392+AgusEchaniz@users.noreply.github.com>
2020-06-18 10:35:02 -0300

    Add files via upload

--- tp/records.py
+++ tp/records.py
@@ -1,2 +1,43 @@
-def ventana():
-	pass
+import json
+
+#def  ventana ():
+#	pass
+
+def crear():
+	datos={'Facil':{},'Medio':{},'Dificil':{}}
+	with open('topten.txt','w') as p:
+		json.dump(datos,p,indent=4)
+	return datos
+	
+def actualizar(dic,nivel):
+	with open('topten.txt','r') as p:
+		datos=json.load(p)
+	if nivel in datos.keys():
+		if  len(datos[nivel])<10 :
+			datos[nivel].update(dic)
+		elif datos[nivel][datos.keys()[-1]]< dic[1]:
+			datos[nivel].pop(datos.keys()[-1])
+			datos[nivel]=dic
+	else :
+		datos[nivel]=dic
+	return datos
+
+def guardarDatos(datos):
+	with open('topten.txt','w') as p:
+		json.dump(datos,p,indent=4)
+
+def imprimir(nivel):
+	with open('topten.txt','r') as p:
+		datos=json.load(p)
+		print(json.dumps(datos,indent=4))
+		
+
+nom='pepe'
+punt=0
+dic={nom:punt}
+nivel='Dificil'
+#datos=crear() #ojo que cada vez que ejecuta borra todo
+datos={}
+datos=actualizar(dic,nivel)
+guardarDatos(datos)
+imprimir(nivel)

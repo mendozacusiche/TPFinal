080fafbbd56d11373302e8f35c851ff12db2ba99
josedrozd <62053648+josedrozd@users.noreply.github.com>
2020-06-22 10:11:03 -0300

    Update ventana_bienvenida.py

--- tp/ventana_bienvenida.py
+++ tp/ventana_bienvenida.py
@@ -1,24 +1,34 @@
 import PySimpleGUI as sg 
 
-def ventana():
-  #sg.theme('Dark Brown 1')
-  sg.ChangeLookAndFeel('LightGreen')
+def ventana(jugador="None"):
 
-  layout = [
-            [sg.Text('Bienvenido a ScrabbleAR! ',size=(40, 1), justification='center', font='Courier 15')],
-            [sg.Image(filename='imagenes/icons.png', pad=(215, 0))],
-            [sg.Text('Por favor, introduzca su nombre ',size=(40, 1), justification='center', font='Courier 15')],
-            [sg.InputText(size=(50, 1),justification='left', font="Helvetica",key="-Nombre-")],
-            [sg.Button('Confirmar'), sg.Button('Salir')],
-          ]
-  window = sg.Window('ScrabbleAR',element_justification='center', resizable= True).Layout(layout)
-  while True:             # Event Loop
-      event, values = window.read()
-      if event in (None, 'Salir'):
-        window.close()
-        return "None"
-      elif (event =="Confirmar") and (values["-Nombre-"] not in ("","Olvido el nombre!")):
-        window.close()
-        return values["-Nombre-"]
-      else:
-        window["-Nombre-"].update("Olvido el nombre!")
+  if jugador != "None":
+    layout = [
+              [sg.Text('Bienvenido a ScrabbleAR! ',size=(40, 1), justification='center', font='Courier 15')],
+              [sg.Image(filename='imagenes/icons.png', pad=(215, 0))],
+              [sg.Text('Bienvenido de nuevo ',size=(40, 1), justification='center', font='Courier 15')],
+              [sg.Ok()]
+            ]
+    window = sg.Window('ScrabbleAR',element_justification='center', resizable= True).Layout(layout)
+    event, values= window.read()
+    window.close()
+
+  else:
+    layout = [
+              [sg.Text('Bienvenido a ScrabbleAR! ',size=(40, 1), justification='center', font='Courier 15')],
+              [sg.Image(filename='imagenes/icons.png', pad=(215, 0))],
+              [sg.Text('Por favor, introduzca su nombre ',size=(40, 1), justification='center', font='Courier 15')],
+              [sg.InputText(size=(50, 1),justification='left', font="Helvetica",key="-Nombre-")],
+              [sg.Button('Confirmar'), sg.Button('Salir')],
+            ]
+    window = sg.Window('ScrabbleAR',element_justification='center', resizable= True).Layout(layout)
+    while True:             # Event Loop
+        event, values = window.read()
+        if event in (None, 'Salir'):
+          window.close()
+          return jugador
+        elif (event =="Confirmar") and (values["-Nombre-"] not in ("","Olvido el nombre!")):
+          window.close()
+          return values["-Nombre-"]
+        else:
+          window["-Nombre-"].update("Olvido el nombre!")

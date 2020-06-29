ad4a240c9f1b2a7b05fec11efb32c825bdb8b592
josedrozd <62053648+josedrozd@users.noreply.github.com>
2020-06-29 15:33:28 -0300

    Update nuevo_juego.py

--- tp/nuevo_juego.py
+++ tp/nuevo_juego.py
@@ -6,8 +6,8 @@ def ventana():
 	columna1=[[sg.Text("tablero")]]
 				
 	columna2=[[sg.Text("descripción configuración predeterminada")],
-			[sg.Button('Configuracion',font=("Impact",10))],
-			[sg.Button('Jugar',font=("Impact",10)), sg.Button('Atras',font=("Impact",10))]
+			[sg.Button('Configuracion',font=("Current",10))],
+			[sg.Button('Jugar',font=("Current",10)), sg.Button('Atras',font=("Current",10))]
 			]
 	
 	layout=[[sg.Column(columna1),sg.Column(columna2)]]

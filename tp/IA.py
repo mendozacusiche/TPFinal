85d28abb743eb48b0328653c51810d41f8a860c3
josedrozd <62053648+josedrozd@users.noreply.github.com>
2020-07-19 16:14:56 -0300

    Update IA.py

--- tp/IA.py
+++ tp/IA.py
@@ -1,17 +1,63 @@
-import jugar, Fichas
+import jugar, Fichas, Tablero
 
 class IA():
 
-	def __init__(self,bolsa):
+	def __init__(self,bolsa,dificultad):
 		nuevas=[]
 		for i in range(7):
 			l=jugar.sacar_letra_bolsa(bolsa)
 			nuevas.append(l)
 		self.__fichas=Fichas.Fichas(nuevas)
 		self.__mi_turno=False
+		self.__dificultad=dificultad
+		self.__cambios_letras=3
 
-	def turno(self):
-		pass
+	def combinaciones(self,combs,pal,marcas,letras,largo):
+		if (largo>0):
+			for i in range(len(letras)):
+				if(not marcas[i]):
+					marcas[i]=True
+					self.combinaciones(combs,pal+letras[i]+" ",marcas,letras,largo-1)
+					marcas[i]=False
+		else:
+			combs.append(pal)
+
+	def buscar_palabra(self):
+		palabra=""
+		marcas=[False for i in range(len(self.__fichas.get_letras()))]
+		n=7
+		while(palabra=="" and n>0):
+			combs=[]
+			self.combinaciones(combs,palabra,marcas,self.__fichas.get_letras(),n)
+			for i in range(len(combs)):
+				pal=""
+				for letra in combs[i].split():
+					pal=pal+letra.lower()
+				if (jugar.evaluar(pal, self.__dificultad)):
+					palabra=combs[i]
+					break
+			n-=1
+		return palabra
+
+	def turno(self,bolsa,window,tablero,puntos):
+		palabra=self.buscar_palabra()
+
+		if (palabra!=""):
+			ok, puntos = tablero.insertar_palabra(palabra,window,puntos)
+			if(ok):
+				for l in palabra.split():
+					i=0
+					while((l != self.__fichas.get_letras()[i]) or (self.__fichas.get_usadas()[i])):
+						i+=1
+					self.__fichas.usar(i)
+					self.__fichas.set_letra(jugar.sacar_letra_bolsa(bolsa),i)
+			for i in range(7):
+				self.__fichas.desusar(i)
+		else:
+			if self.__cambios_letras > 0:
+				jugar.cambiar_fichas(window,self.__fichas,bolsa,tablero,True)
+				self.__cambios_letras-=1
+		return puntos
 
 	def get_mi_turno(self):
 		return self.__mi_turno
@@ -21,3 +67,6 @@ class IA():
 
 	def get_fichas(self):
 		return self.__fichas
+
+	def get_dificultad(self):
+		return self.__dificultad

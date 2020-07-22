c6416b0317428b0b7bce975fcf722453878306ce
AgusEchaniz <62100392+AgusEchaniz@users.noreply.github.com>
2020-07-22 14:30:17 -0300

    Add files via upload

--- tp/records.py
+++ tp/records.py
@@ -5,15 +5,19 @@ def  ventana ():
 	opciones=('Facil','Medio','Dificil')
 	layout=[
 		[sg.Text('Elija el nivel para ver el TOP TEN: ')],
-		[sg.Listbox(opciones,size=(15,len(opciones)))],
-		[sg.Button('OK')]
+		[sg.Listbox(opciones,size=(15,len(opciones))),sg.Button('OK')],
+		[sg.Listbox( values={}, key='RECORDS', size= (60,10) )],
+		[sg.Button('Salir')]
 		]
 	window=sg.Window('record',layout)
-	event,values=window.Read()
-	if event=='OK':
-		#print(values[0][0])
-		imprimir(values[0][0])
-		window.close()
+	while True:
+		event,values=window.Read()
+		if event=='OK':
+			#print(values[0][0])
+			imprimir(values[0][0],window)
+		if event=='Salir':
+			break
+	window.close()
 
 
 def crear():
@@ -40,13 +44,21 @@ def guardarDatos(datos):
 	with open('topten.txt','w') as p:
 		json.dump(datos,p,indent=4)
 
-def imprimir(nivel):
+def imprimir(nivel,win):
 	with open('topten.txt','r') as p:
 		datos=json.load(p)
-		try:
-			print(json.dumps(datos[nivel],indent=4))
-		except KeyError:
-			print('No hay registros del nivel seleccionado')
+	try:
+		l=[]
+		tup=()
+		for elem in datos[nivel]:
+			tup=(elem,datos[nivel][elem])
+			l.append(tup)
+		win['RECORDS'].update(map(lambda x: "{}: {}".format(x[0], x[1]), l))
+	except KeyError:
+		sg.popup('No hay registros del nivel seleccionado')
+
+			
+
 
 #nom='pepe'
 #punt=0

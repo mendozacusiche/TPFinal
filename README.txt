7af2cc659ff3c76111b018e3d0d97be84307e0f6
AgusEchaniz <62100392+AgusEchaniz@users.noreply.github.com>
2020-08-03 18:25:51 -0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,12 @@
+# TPFinal
+TPFinal seminario
+GRUPO 20: María Agustina Echaniz - Abel Mendoza Cusiche - José Iván Drozd 
+
+Requisitos:
+-Usar python 3.6.0
+-Resolución recomenda de la pantalla 1366x768 o superior
+-Ejecutar directamente desde el archivo ScrabbleAr.py desde un intérpete , por ejemplo Geany
+
+Creditos:
+-Imagen utilizada para el cartel de bienvenida : https://www.flaticon.es/iconos-gratis/rayo
+-Las imagenes computerlog.png y playerlogo.png fueron creadas por el alumno José Iván Drozd
---
+++ ScrabbleAR.py
@@ -0,0 +1,43 @@
+import PySimpleGUI as sg
+import os
+from codigos import configuracion,nuevo_juego,jugar,records
+
+
+def main(args):
+	#sg.theme('DarkTeal8') #'BlueMono' 'BluePurple' 'DarkAmber'
+	sg.theme('BlueMono')
+	layout=[[sg.Button('Nuevo Juego',font=("Arial Black",12), size=(20, 0))],
+			[sg.Button('Cargar Juego',font=("Arial Black",12), size=(20, 0), disabled=True)],
+			[sg.Button("Records",font=("Arial Black",12), size=(20, 0))],
+			[sg.Button('Salir',font=("Arial Black",12),size=(20, 0))]]
+	if os.path.isfile("archivos/guardado.json"): #si existe un juego guardado se habilita el botón cargar juego
+		layout[1]=[sg.Button('Cargar Juego',font=("Arial Black",12), size=(20, 0), disabled=False)]
+
+	window=sg.Window('Scrabble',layout)
+
+	while True:
+		event,values=window.Read()
+
+		if (event== 'Nuevo Juego'):
+			window.Hide()
+			nuevo_juego.ventana()
+			if os.path.isfile("archivos/guardado.json"):
+				window["Cargar Juego"].update(disabled=False) #si luego de jugar se guarda la partida se habilita el botón Cargar Juego
+			window.UnHide()
+		elif (event=='Cargar Juego'):
+			window.Hide()
+			jugar.juego(True)
+			window.UnHide()
+		elif (event=="Records"):
+			window.Hide()
+			records.ventana()
+			window.UnHide()
+		elif (event == sg.WIN_CLOSED or event == "Salir"):
+			break
+			
+	window.close()
+	
+	
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))

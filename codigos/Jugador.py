5c28e1e861686cf21c2e3b27b73f507374547c12
AgusEchaniz <62100392+AgusEchaniz@users.noreply.github.com>
2020-08-03 18:27:42 -0300

    Add files via upload

---
+++ codigos/Jugador.py
@@ -0,0 +1,41 @@
+from codigos import jugar, Fichas
+
+class Jugador():
+
+	def __init__ (self,nombre,fichas,mi_turno,primer_turno=True,cambios=3,puntos=0):
+		self.__nombre=nombre
+		self.__fichas= fichas
+		self.__mi_turno=mi_turno
+		self.__primer_turno=primer_turno
+		self.__cambios=cambios
+		self.__puntos=puntos
+
+	def get_nombre(self):
+		return self.__nombre
+
+	def get_fichas(self):
+		return self.__fichas
+
+	def get_mi_turno(self):
+		return self.__mi_turno
+
+	def set_mi_turno(self,mi_turno):
+		self.__mi_turno=mi_turno
+
+	def get_primer_turno(self):
+		return self.__primer_turno
+
+	def set_primer_turno(self,b):
+		self.__primer_turno=b
+
+	def get_cambios(self):
+		return self.__cambios
+
+	def set_cambios(self,c):
+		self.__cambios=c
+
+	def get_puntos(self):
+		return self.__puntos
+
+	def set_puntos(self,puntos):
+		self.__puntos=puntos
---
+++ codigos/nuevo_juego.py
@@ -0,0 +1,55 @@
+import PySimpleGUI as sg
+from codigos import configuracion, jugar
+import json
+
+def ventana():	
+	try:
+		with open("archivos/config.json","r") as archivo:
+			config= json.load(archivo)
+	
+		dificultad=config["dificultad"]
+		if dificultad=='Facil':
+			imag=sg.Image(filename='imagenes/tabfacil.png',key='-imagen-')
+			descr="""Nivel fácil: 			
+Palabras permitidas: sustantivos, adjetivos y verbos. 
+Tamaño del tablero: 23x23."""
+		elif dificultad=='Medio':
+			imag=sg.Image(filename='imagenes/tabmedio.png',key='-imagen-')
+			descr="""Nivel medio: 			
+Palabras permitidas: adjetivos y verbos. 
+Tamaño del tablero: 19x19."""
+		else:
+			imag=sg.Image(filename='imagenes/tabdificil.png',key='-imagen-')
+			descr="""Nivel dificil: 			
+Palabras permitidas: son elegidas al azar entre adjetivos y verbos. 
+Tamaño del tablero: 15x15."""
+
+		columna1=[[imag]]
+				
+	except FileNotFoundError as ex:
+		sg.popup("No se encontro el archivo config.json")
+		columna1=[[sg.Text('No hay tablero actual')]]
+		descr='No hay descripción del nivel actual'
+				
+	columna2=[[sg.Text(descr,key='-descr-',font=("Arial Black",10), size = (35, 0),justification='ljust')],
+			[sg.Button('Configuración',key='Configuracion',font=("Arial Black",12), size =(29, 0))],
+			[sg.Button('Jugar',font=("Arial Black",12), size=(13, 0)), sg.Button('Atrás',key='Atras',font=("Arial Black",12), size=(13,0),pad=(20,0))]
+			]
+	
+	layout=[[sg.Column(columna1),sg.Column(columna2)]]
+	
+	window=sg.Window('ScrabbleAR',layout)
+	
+	while True:
+		event,values=window.Read()
+		
+		if (event== 'Jugar'):
+			window.close()
+			jugar.juego()
+		elif (event=='Configuracion'):
+			window.Hide()
+			configuracion.ventana(window)
+			window.UnHide()
+		elif (event == sg.WIN_CLOSED or event == "Atras"):
+			window.close()
+			break

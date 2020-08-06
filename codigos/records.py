00fe8b58233976ff6dc8e585937d912c31684a7f
AgusEchaniz <62100392+AgusEchaniz@users.noreply.github.com>
2020-08-06 11:52:42 -0300

    Update records.py

--- codigos/records.py
+++ codigos/records.py
@@ -13,7 +13,7 @@ def  ventana ():
 	window=sg.Window('record',layout)
 	while True:
 		event,values=window.Read()
-		if event == "-dif-":
+		if event=="-dif-":
 			imprimir(values["-dif-"],window)
 		if event in (None,'Salir'):
 			break
@@ -34,7 +34,6 @@ def actualizar(nombre,puntaje,nivel,fecha):
 				if (len(datos[nivel])<10):
 					datos[nivel][nombre]=(puntaje,f)
 				else:
-					#print(len(datos[nivel]))
 					minimo=min(datos[nivel], key=datos[nivel].get)
 					if puntaje>datos[nivel][minimo][0]:
 						datos[nivel].pop(minimo)
@@ -46,7 +45,6 @@ def actualizar(nombre,puntaje,nivel,fecha):
 		guardarDatos(datos)
 	except FileNotFoundError:
 		sg.popup('No se encontro el archivo topten.json')
-
 
 def guardarDatos(datos):   #no uso manejo de excepciones porque ya las uso cuando lo llamo 
 	'''Guardo los datos en topten.son que fueron generados en actualizar, ya 
@@ -61,7 +59,7 @@ def imprimir(nivel,win):
 			datos=json.load(p)
 
 		try:
-			lista = sorted(datos[nivel].items(), key=lambda x: x[1][0],reverse=True)
+			lista=sorted(datos[nivel].items(), key=lambda x: x[1][0],reverse=True)
 
 			win['RECORDS'].update(map(lambda x: "{}. {} {}: {}".format(lista.index(x)+1,x[1][1],x[0], x[1][0]),lista))
 		except KeyError:
@@ -69,7 +67,6 @@ def imprimir(nivel,win):
 	except FileNotFoundError:
 		sg.popup('No se encontro el archivo topten.json')
 		
-
-if __name__ == '__main__':
+if __name__=='__main__':
 	sg.theme('BlueMono')
 	sg.popup('Por favor ejecute ScrabbleAR.py',title='')

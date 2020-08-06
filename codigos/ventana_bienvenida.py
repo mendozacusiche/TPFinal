826474ef6b45d7723fe858e1f354244814c56e1c
josedrozd <62053648+josedrozd@users.noreply.github.com>
2020-08-06 11:27:05 -0300

    Update ventana_bienvenida.py

--- codigos/ventana_bienvenida.py
+++ codigos/ventana_bienvenida.py
@@ -4,32 +4,32 @@ from tkinter import *
 def ventana(jugador="None"):
 	''' Creación ventana de bienvenida ''' 
 	try:
-		if jugador != "None":
-			layout = [
+		if jugador!="None":
+			layout=[
 					[sg.Text('Bienvenido a ScrabbleAR! ',size=(40, 1), justification='center', font='Courier 15')],
 					[sg.Image(filename='imagenes/icons.png', pad=(215, 0))],
 					[sg.Text('Bienvenido de nuevo '+jugador,size=(40, 1), justification='center', font='Courier 15')],
 					[sg.Ok()]
 					]
-			window = sg.Window('ScrabbleAR',element_justification='center', resizable= True).Layout(layout)
-			event, values= window.read()
+			window=sg.Window('ScrabbleAR',element_justification='center', resizable= True).Layout(layout)
+			event, values=window.read()
 			window.close()
 
 		else:
-			layout = [
+			layout=[
 					[sg.Text('Bienvenido a ScrabbleAR! ',size=(40, 1), justification='center', font='Courier 15')],
 					[sg.Image(filename='imagenes/icons.png', pad=(215, 0))],
 					[sg.Text('Por favor, introduzca su nombre ',size=(40, 1), justification='center', font='Courier 15')],
 					[sg.InputText(size=(50, 1),justification='left', font="Helvetica",key="-Nombre-")],
 					[sg.Button('Confirmar',font=("Arial Black",11), size=(25, 0))],
 					]
-			window = sg.Window('ScrabbleAR',element_justification='center', resizable= True).Layout(layout)
+			window=sg.Window('ScrabbleAR',element_justification='center', resizable= True).Layout(layout)
 			while True:             # Event Loop
 				event, values = window.read()
-				if event == None:
+				if event==None:
 					window.close()
 					return jugador
-				elif (event =="Confirmar") and (values["-Nombre-"] not in ("","Olvido el nombre!")):
+				elif (event=="Confirmar") and (values["-Nombre-"] not in ("","Olvido el nombre!")):
 					window.close()
 					return values["-Nombre-"]
 				else:
